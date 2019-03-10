533bd8617031a9ed8a0a93a383d699afdd6ba18f
Prajwal Gowda <pgowda@itemis.de>
2019-03-10 20:12:14 +0100

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,5 +1,20 @@
-#!/usr/bin/env groovy
-def call(testVar){
-   println testVar
+def call(body) {
+
+    // evaluate the body block, and collect configuration into the object
+
+    def params= [:]
+
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+
+    body.delegate = params
+
+    body()
+
+ 
+
+    echo params.someParam
+
 }
+
+ 
    
