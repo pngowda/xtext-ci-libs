f435dfeeea67e1a6cc48171cd25dfde5b85cc04e
Prajwal Gowda <prajwal.n85@gmail.com>
2020-02-09 21:38:38 +0100

    Update mkobitVar.groovy

--- vars/mkobitVar.groovy
+++ vars/mkobitVar.groovy
@@ -1,6 +1,8 @@
 def runMyPython() {
   final pythonContent = libraryResource('com/mkobit/sharedlib/my_file.py')
+  final pythonContent2 = libraryResource('com/mkobit/sharedlib/example.py')
   // There are definitely better ways to do this without having to write to the consumer's workspace
   writeFile(file: 'my_file.py', text: pythonContent)
+  writeFile(file: 'example.py', text: pythonContent2)
   sh('chmod +x my_file.py && ./my_file.py')
 }
